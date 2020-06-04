4cd24398b5428e25668309a405ad3f42e76a468b
mike8790 <michael_longley87@gmail.com>
2020-06-04 21:04:36 +0100

    .

--- dartel_template_base_batch_complete.m
+++ dartel_template_base_batch_complete.m
@@ -1,29 +1,33 @@
-% List of open inputs
-% Run Dartel (create Templates): Images - cfg_files
-% Run Dartel (create Templates): Images - cfg_files
+function cluster_dartel_template_batch(subject_number);
 
-clear all
-
+    disp('inside the main matlab function');
+    disp('Processing Subject');
+    disp(subject_number);
+    
 % set path
-addpath /home/ugjt015/spm12
+addpath /usr/local/apps/psycapps/spm/spm12-r7487
 
 cd '/MRIWork/MRIWork06/nr/michael_longley/CanCAM/raw_data/anat/'
 rc1_list = importdata('rc1list.txt');
 rc2_list = importdata('rc2list.txt');
+rc3_list = importdata('rc3list.txt');
 
 rc1_list = rc1_list.';
 rc1_list = cellstr(rc1_list);
 rc2_list = rc2_list.';
 rc2_list = cellstr(rc2_list);
+rc3_list = rc3_list.';
+rc3_list = cellstr(rc3_list);
 
-nrun = 1; % enter the number of runs here
+jobs = repmat(jobfile, 1, 1);
+inputs = cell(3, 1);
+
 % images = 3;
 jobfile = {'/MRIWork/MRIWork06/nr/michael_longley/10_sub_preproc_test/dartel_template_base_batch_complete_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(2, nrun);
-for crun = 1:nrun
-    inputs{1,crun} = rc1_list(:); % Run Dartel (create Templates): Images - cfg_files
-    inputs{2,crun} = rc2_list(:); % Run Dartel (create Templates): Images - cfg_files
-end
+
+    inputs{1,1} = rc1_list(:); % Run Dartel (create Templates): Images - cfg_files
+    inputs{2,1} = rc2_list(:); % Run Dartel (create Templates): Images - cfg_files
+    inputs{3,1} = rc3_list(:); % Run Dartel (create Templates): Images - cfg_files
+
 spm('defaults', 'FMRI');
 spm_jobman('run', jobs, inputs{:});
