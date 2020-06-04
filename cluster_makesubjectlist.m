cc0cfe9bb3ddbc9b5a2998a68153a9876f0cbff1
Mike8790 <michael.lonley87@gmail.com>
2020-06-04 16:22:12 +0100

    cluster_makesubjectlist.m

--- cluster_makesubjectlist.m
+++ cluster_makesubjectlist.m
@@ -1,9 +1,9 @@
 cd ('/MRIWork/MRIWork06/nr/michael_longley/CanCAM/raw_data/epi_rest')
 
 subject_number = importdata('participants.tsv');
-subject_number = (subject_number.textdata(11:15,1));
+subject_number = (subject_number.textdata(1:40,1));
 subject_number = subject_number.';
 
-cd('/MRIWork/MRIWork06/nr/michael_longley/10_sub_preproc_test')
+cd('/MRIWork/MRIWork06/nr/michael_longley/40_sub_run')
 %writecell(subject_number, 'subject_number.dat')
 writecell(subject_number, 'subject_number.txt');
